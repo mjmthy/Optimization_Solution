a67698f326c4af338979a34a49a183a8fa305193
Jiamin Ma <mjmthy@163.com>
2020-06-17 15:19:58 +0800

    remove license header

--- cpuburn_enhance/srcs/cpuburn_sysfs.c
+++ cpuburn_enhance/srcs/cpuburn_sysfs.c
@@ -1,20 +1,3 @@
-/*
- * drivers/amlogic/cpuburn/cpuburn_sysfs.c
- *
- * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- */
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/cdev.h>
